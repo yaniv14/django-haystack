4459c4e5f958401bafb09e2b6914f7be0ddaed99
Yaniv Mirel <yanivmirel@gmail.com>
2017-12-19 13:40:41 +0200

    Update indexes.py

--- haystack/indexes.py
+++ haystack/indexes.py
@@ -443,7 +443,7 @@ class ModelSearchIndex(SearchIndex):
             return True
 
         # Ignore certain fields (AutoField, related fields).
-        if field.primary_key or getattr(field, 'rel'):
+        if field.primary_key or getattr(field, 'rel', False):
             return True
 
         return False
